8eb27039b2188c929cc3a7144c87ee042d3472d4
samuel akande <samuel@softcom.ng>
2021-09-10 22:06:31 +0100

    heroku deploy

--- src/components/education/Package.js
+++ src/components/education/Package.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Package = () => {
-    return (
-        <div className='p-8'>
-            
-        </div>
-    )
-}
+  return (
+    <div className="p-8 bg-intro">
+      <div className='my-4 mb-8'>
+        <h2>Learn how to Trade and Invest in Crypto</h2>
+        <h3>
+          Crypto has a steep learning curve but we are here to flatten it for
+          you
+        </h3>
+      </div>
+      <div className='grid md:grid-cols-2 gap-4'>
+          <div className='shadow'>
+                <div className='border-b p-2 border-gray-300'>
+                STANDARD
+                </div>
+                <div className='p-2'>
+                    <h3>Pay $250 Monthly for 3 Months </h3>
+                </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default Package
+export default Package;
